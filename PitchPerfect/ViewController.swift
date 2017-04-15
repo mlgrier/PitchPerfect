820b869dca8c01a88aa0f39c4a34b12f48e280bf
Marco Grier <mlgrier@gmail.com>
2017-04-14 21:59:43 -0500

    Added Images

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,10 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillappear called")
     }
 
     override func didReceiveMemoryWarning() {
